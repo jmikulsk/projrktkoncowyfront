48513450b766a34784e86195f491d10cd9e8c5a0
KUBA <jmikulsk92>
2020-05-03 12:45:47 +0200

    test chart

--- src/app/measurement/chart/chart.component.ts
+++ src/app/measurement/chart/chart.component.ts
@@ -1,15 +1,105 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import { Chart } from 'chart.js';
+import {HttpClientService} from '../../http-client.service';
+import {Pomiar} from '../measure';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
-  styleUrls: ['./chart.component.css']
+  styleUrls: ['./chart.component.css'],
 })
 export class ChartComponent implements OnInit {
+  constructor(private httpClient: HttpClientService) { }
 
-  constructor() { }
+  public name = 'Angular   6';
+  public canvas: any;
+  public ctx: any;
+  @ViewChild('mychart') public mychart;
+  public pomiary: Pomiar[] = [];
+  public test: number;
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+
+    this.getPomiary();
+    this.test = this.pomiary[100].id;
+    console.log(this.test);
+  }
+
+  public ngAfterViewInit() {
+    this.canvas = this.mychart.nativeElement;
+    this.ctx = this.canvas.getContext('2d');
+
+    const myChart = new Chart(this.ctx, {
+      type: 'line',
+
+      data: {
+        datasets: [{
+          label: 'Temperatura',
+          backgroundColor: 'rgba(255, 99, 132,0.4)',
+          borderColor: 'rgb(255,222,37)',
+          fill: true,
+          data: [
+            {x: new Date (1995,11,11,3,4,5).getFullYear(), y: this.pomiary[this.pomiary.length - 200].temperatura},
+            {x:  new Date(1996,11,11,3,4,5).getFullYear(), y: this.pomiary[this.pomiary.length - 99].temperatura},
+            {x:  new Date(1997,11,11,3,4,5).getFullYear(), y: this.pomiary[this.pomiary.length - 98].temperatura},
+            // {x: this.pomiary[this.pomiary.length-97].id, y: this.pomiary[this.pomiary.length-9].temperatura},
+            // {x: this.pomiary[this.pomiary.length-96].id, y: this.pomiary[this.pomiary.length-96].temperatura},
+            // {x: this.pomiary[this.pomiary.length-95].id, y: this.pomiary[this.pomiary.length-95].temperatura},
+            // {x: this.pomiary[this.pomiary.length-94].id, y: this.pomiary[this.pomiary.length-94].temperatura},
+            // {x: this.pomiary[this.pomiary.length-93].id, y: this.pomiary[this.pomiary.length-93].temperatura},
+            // {x: this.pomiary[this.pomiary.length-92].id, y: this.pomiary[this.pomiary.length-92].temperatura},
+            // {x: this.pomiary[this.pomiary.length - 91].id, y: this.pomiary[this.pomiary.length - 91].temperatura},
+
+            //             // {x: 3000, y: 5},
+            //             // {x: 3400, y: 4.75},
+            //             // {x: 3600, y: 4.75},
+            //             // {x: 5200, y: 6},
+            //             // {x: 6000, y: 9},
+            //             // {x: 7222, y: 10},
+          ],
+        }],
+      },
+      options: {
+        responsive: true,
+        title: {
+          display: true,
+          text: 'Temperatura',
+        },
+        scales: {
+          xAxes: [{
+            type: 'linear',
+            position: 'bottom',
+
+            // ticks: {
+            //   userCallback: function() {
+            //
+            //     return 'dupa';
+            // //   }
+            // },
+            scaleLabel: {
+              labelString: 'Data',
+              display: true,
+            },
+          }],
+          yAxes: [{
+            type: 'linear',
+            // ticks: {
+            //   userCallback: function(tick) {
+            //     return tick.toString() + 'm';
+            //   }
+            // },
+            scaleLabel: {
+              labelString: 'Temperatura Celcjusze',
+              display: true,
+            },
+          }],
+        },
+      },
+    });
+  }
+
+  public getPomiary() {
+    this.httpClient.getPomiary().subscribe((p) => this.pomiary = p);
   }
 
 }
--- src/app/measurement/measure.ts
+++ src/app/measurement/measure.ts
@@ -1,8 +1,47 @@
 export class Pomiar {
-  id: number;
-  data: string;
-  temperatura: number;
-  wilgotnosc: number;
-  info: string;
+  private _id: number;
+  private _data: string;
+  private _temperatura: number;
+  private _wilgotnosc: number;
+  private _info: string;
   // info: string;
+  get id(): number {
+    return this._id;
+  }
+
+  set id(value: number) {
+    this._id = value;
+  }
+
+  get data(): string {
+    return this._data;
+  }
+
+  set data(value: string) {
+    this._data = value;
+  }
+
+  get temperatura(): number {
+    return this._temperatura;
+  }
+
+  set temperatura(value: number) {
+    this._temperatura = value;
+  }
+
+  get wilgotnosc(): number {
+    return this._wilgotnosc;
+  }
+
+  set wilgotnosc(value: number) {
+    this._wilgotnosc = value;
+  }
+
+  get info(): string {
+    return this._info;
+  }
+
+  set info(value: string) {
+    this._info = value;
+  }
 }
