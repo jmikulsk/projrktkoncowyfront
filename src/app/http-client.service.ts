dedadad81ea1b0463b5c64e806245faab4c0f008
KUBA <jmikulsk92>
2020-05-20 22:18:49 +0200

    poprawa Edytora

--- src/app/http-client.service.ts
+++ src/app/http-client.service.ts
@@ -35,7 +35,7 @@ export class HttpClientService {
 
   savePomiar(pomiar: Pomiar) {
     const headers=new HttpHeaders({Authorization: 'Basic '+btoa(this.userName + ":" + this.Passwordd)});
-    return this.httpClient.post(`${BASE_URL}/pomiary/zapisz`,  JSON.stringify(pomiar));
+    return this.httpClient.post(`https://projektkoncowyback.herokuapp.com/noAuth/zapisz`,  JSON.stringify(pomiar));
   }
   public login(username : string,password : string){
     const headers=new HttpHeaders({Authorization: 'Basic '+btoa(username + ":" + password)});
