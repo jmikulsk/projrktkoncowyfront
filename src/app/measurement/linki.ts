158669e83e9c7cd9bd97afc4e132688ea11abc37
KUBA <jmikulsk92>
2020-05-22 22:37:27 +0200

    Dodanie LINKOWNI

--- src/app/measurement/linki.ts
+++ src/app/measurement/linki.ts
@@ -1,30 +1,7 @@
 export class Linki {
-  private _id: number;
-  private link: string;
-  private informacja: string;
+  public id: number;
+  public link: string;
+  public informacja: string;
 
-
-  get id(): number {
-    return this._id;
-  }
-
-  set id(value: number) {
-    this._id = value;
-  }
-
-  get linkk(): string {
-    return this.link;
-  }
-
-  set linkk(value: string) {
-    this.link = value;
-  }
-
-  get informacjaa(): string {
-    return this.informacja;
-  }
-
-  set informacjaa(value: string) {
-    this.informacja = value;
-  }
 }
+
